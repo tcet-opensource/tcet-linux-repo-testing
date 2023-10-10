4033d8428c29c323cea81a48966f2dbd872f2776
Akash6222 <akashdubey25274@gmail.com>
2023-10-10 20:07:21 +0530

    added tcet-linux-repo-testing.db.sig

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -3,8 +3,21 @@
 rm tcet-linux-repo-testing*
 
 echo "repo-add"
-repo-add -n -R tcet-linux-repo-testing.db.tar.gz *.pkg.tar.zst
-sleep 5
+repo-add -s -n -R tcet-linux-repo-testing.db.tar.gz *.pkg.tar.zst
+sleep 1
+
+rm tcet-linux-repo-testing.db
+rm tcet-linux-repo-testing.db.sig
+
+rm tcet-linux-repo-testing.files
+rm tcet-linux-repo-testing.files.sig
+
+mv tcet-linux-repo-testing.db.tar.gz tcet-linux-repo-testing.db
+mv tcet-linux-repo-testing.db.tar.gz.sig tcet-linux-repo-testing.db.sig
+
+mv tcet-linux-repo-testing.files.tar.gz tcet-linux-repo-testing.files
+mv tcet-linux-repo-testing.files.tar.gz.sig tcet-linux-repo-testing.files.sig
+
 
 
 echo "####################################"
