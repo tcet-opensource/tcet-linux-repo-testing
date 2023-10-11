c4d5b6a96dbfa44a640716c574a133ff902cd940
0xAtharv <atharva.a.vartak@gmail.com>
2023-10-12 01:35:21 +0530

    updated update repo script

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -4,20 +4,7 @@ rm tcet-linux-repo-testing*
 
 echo "repo-add"
 repo-add -s -n -R tcet-linux-repo-testing.db.tar.gz *.pkg.tar.zst
-sleep 1
-
-rm tcet-linux-repo-testing.db
-rm tcet-linux-repo-testing.db.sig
-
-rm tcet-linux-repo-testing.files
-rm tcet-linux-repo-testing.files.sig
-
-mv tcet-linux-repo-testing.db.tar.gz tcet-linux-repo-testing.db
-mv tcet-linux-repo-testing.db.tar.gz.sig tcet-linux-repo-testing.db.sig
-
-mv tcet-linux-repo-testing.files.tar.gz tcet-linux-repo-testing.files
-mv tcet-linux-repo-testing.files.tar.gz.sig tcet-linux-repo-testing.files.sig
-
+sleep 5
 
 
 echo "####################################"
